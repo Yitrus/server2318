cd2ef022abc4ca73e6b3b06ac0de2f27a7ccf4dc
xmu <citrusyi@163.com>
2024-04-28 18:42:12 +0800

    ML的实验结果

--- scripts_memtis/run.sh
+++ scripts_memtis/run.sh
@@ -2,26 +2,26 @@
 
 BENCHMARKS="train" 
 # DRAM_SIZE="5GB 14GB 31GB 40GB 47GB 55GB"
-DRAM_SIZE="25GB"
+DRAM_SIZE="23GB 34GB 45GB"
 dmesg -c 
-
-# for BENCH in ${BENCHMARKS};
-# do
-#     for NR in ${DRAM_SIZE};
-#     do
-#         for i in {1..2};
-#         do
-# 	    # ./bench_scripts/run_balance.sh -B ${BENCH} -V "xx_${i}"
-#         ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V "${NR}-${i}"
-#         # rm kdd12*
-#         done
-#     done
-# done
 
 for BENCH in ${BENCHMARKS};
 do
     for NR in ${DRAM_SIZE};
     do
-        ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V "${NR}basic"
+        for i in {1..2};
+        do
+	    # ./bench_scripts/run_balance.sh -B ${BENCH} -V "xx_${i}"
+        ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V "${NR}-${i}"
+        rm kdd12*
+        done
     done
 done
+
+# for BENCH in ${BENCHMARKS};
+# do
+#     for NR in ${DRAM_SIZE};
+#     do
+#         ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V "${NR}basic"
+#     done
+# done
