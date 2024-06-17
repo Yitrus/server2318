48c1d891809902d78798b47bcf32bad6daf94f70
Yitrus <citrusyi@163.com>
2024-06-17 16:29:04 +0800

    新的实验数据

--- scripts_rl/run.sh
+++ scripts_rl/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-BENCHMARKS="masim_s1 masim_s2 masim_s3 masim_s4 masim_s5 masim_s6" 
-# BENCHMARKS="train" 
-# DRAM_SIZE="46592MB"
-DRAM_SIZE="16GB"
+# BENCHMARKS="XSBencch cc" 
+BENCHMARKS="train" 
+DRAM_SIZE="18637MB 31027MB 46592MB 62157MB"
+# DRAM_SIZE="16GB"
 dmesg -c 
 
 # for BENCH in ${BENCHMARKS};
@@ -13,18 +13,20 @@ dmesg -c
 #         for i in {1..2};
 #         do
 # 	    # ./bench_scripts/run_balance.sh -B ${BENCH} -V "xx_${i}"
-#         ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V "${NR}-${i}"
+#         ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V ${NR}-v42-${i}
 #         # rm kdd12*
 #         done
 #     done
 # done
 
+# killall -9 run_qt.py
+
 for BENCH in ${BENCHMARKS};
 do
     for NR in ${DRAM_SIZE};
     do
-        ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V v41-newtrain
-        # killall run.py
-        sleep 20
+        ./bench_scripts/run_node.sh -B ${BENCH} -R ${NR} -V ${NR}-v42-bw
+        killall -9 pcm-memory
+        sleep 10
     done
 done
